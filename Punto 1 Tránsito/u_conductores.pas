509f573399cb51e551e3f54b96a8276a34ad5466
Wenceslao-x <ColasoWenceslao@Gmail.com>
2023-12-13 17:40:27 -0300

    Modificacion minima

--- Punto 1 Tránsito/u_conductores.pas
+++ Punto 1 Tránsito/u_conductores.pas
@@ -7,32 +7,33 @@ Uses crt, utilidades;
 
 Const 
   N = 20;
-
+  rutaCDNI = 'e:\ArchivoConductoresDNI.DAT';
+  rutaCApynom = 'e:\ArchivoConductoresApynom.DAT';
+  rutaCMaster = 'e:\ArchivoConductoresMaster.DAT';
 Type 
   t_dato = Record
     DNI: string[8];
     ApyNom: string[50];
     Nacimiento: string[10];
     Telefono: string[10];
-    //Ej 3446366096
+    //Ej 3446366096 
     Email: string[50];
-    Scoring: 0..20;
+    Scoring: smallint;//Pueden ser negativos --> por eso no es de 0..20;
     Habilitado: boolean;
     Fecha_habilitacion: string[10];
     cant_reincidencias: 0..N;
   End;
   t_archivo = file Of t_dato;
-
 Var 
   archivo: t_archivo;
-
 Implementation
 
 //ABMC
 Procedure alta();
 //agrega un conductor
 Begin
-  reset(archivo)
+  reset(archivo);
+  
 End;
 Procedure baja();
 Begin
--- Punto 1 Tránsito/u_infracciones.pas
+++ Punto 1 Tránsito/u_infracciones.pas
@@ -15,13 +15,32 @@ Type
   t_archivo = file Of t_dato;
 
 Implementation
+procedure CargaInfra(var dato:t_dato);
+begin
+  //Datos de la infraccion, cargar el t_dato
+  writeln('Ingrese el DNI: ');
+  readln(t_dato.dni);
+  writeln('Ingrese la fecha de la infraccion: ');
+  readln(t_dato.Fecha_infraccion);
+  writeln('tipo infracción: ');
+  readln(t_dato.tipo);
+  writeln('Puntaje que se descuenta: ');
+  readln(t_dato.puntos);
+end;
+procedure GuardarInfra(var Arch:t_archivo; var pos:cardinal; dato:t_dato);
+begin
+  //Escribir el archivo de infracciones
+  seek(Arch, pos);
+  write(Arch, dato);
+end;
 Procedure alta();
+VAR
+  dato:t_dato;
+  pos:cardinal;
 Begin
-
-End;
-Procedure baja();
-Begin
-
+  CargaInfra(dato);
+  pos:= filesize(InfraMaster);
+  GuardarInfra(InfraMaster, pos, dato);
 End;
 Procedure modificar();
 Begin
